c4288ac91a096e63ae94e67f3e3ae7eb8cee0928
Haripriya-511 <kataripriya1@gmail.com>
2025-04-08 21:28:47 +0530

    commit checking

--- springsecurityproject/src/main/java/com/security/springsecurityproject/controller/WelcomeController.java
+++ springsecurityproject/src/main/java/com/security/springsecurityproject/controller/WelcomeController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1")
 public class WelcomeController {
     @GetMapping("/welcome")
     public String sayWelcome(){
